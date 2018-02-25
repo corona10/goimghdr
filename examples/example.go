1b61d6315b58267cea395fa879a4be9a7e03282f
Dong-hee Na <donghee.na92@gmail.com>
2018-02-26 02:53:59 +0900

    goimghdr/example: formatting.

--- examples/example.go
+++ examples/example.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-        "fmt"
-        "os"
+	"fmt"
+	"os"
 
-        "github.com/corona10/goimghdr"
+	"github.com/corona10/goimghdr"
 )
 
 func main() {
-        path1 := "../imgData/gopher.jpeg"
-        path2 := "../imgData/gopher.png"
-        ret1, _ := goimghdr.What(path1)
-        fmt.Printf("%s: %s\n", path1, ret1)
-        f, _ := os.Open(path2)
-        ret2, _ := goimghdr.WhatFromReader(f)
-        fmt.Printf("%s: %s\n", path2, ret2)
-        f.Close()
+	path1 := "../imgData/gopher.jpeg"
+	path2 := "../imgData/gopher.png"
+	ret1, _ := goimghdr.What(path1)
+	fmt.Printf("%s: %s\n", path1, ret1)
+	f, _ := os.Open(path2)
+	ret2, _ := goimghdr.WhatFromReader(f)
+	fmt.Printf("%s: %s\n", path2, ret2)
+	f.Close()
 }
